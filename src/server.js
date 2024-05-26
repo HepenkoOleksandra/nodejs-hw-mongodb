7239cbc70db67c088f104ca089639cc9994b5925
HepenkoOleksandra <gepenkoalex@gmail.com>
2024-05-26 16:32:01 +0300

    create server and middlewares

--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import pino from "pino-http";
 import cors from 'cors';
+import { env } from './utils/env.js';
+import { ENV_VARS } from './constants/index.js';
+import { errorHandlerMiddleware } from './middlewares/errorHandlerMiddleware.js';
+import { notFoundMiddleware } from './middlewares/notFoundMiddleware.js';
 
-const PORT = 3000;
+
+const PORT = Number(env(ENV_VARS.PORT, 3005));
 
 export const setupServer = () => {
     const app = express();
@@ -27,15 +32,9 @@ export const setupServer = () => {
         });
     });
 
-    app.use('*', (req, res, next) => {
-        res.status(404).json({
-            message: 'Not found',
-        });
-    });
+    app.use('*', notFoundMiddleware);
 
-    app.use((error, req, res, next) => {
-        res.status(500).send(error.message);
-    });
+    app.use(errorHandlerMiddleware);
 
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
