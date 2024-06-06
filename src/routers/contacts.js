afca5548397bde595ebfe271cb4021e53bc296f9
HepenkoOleksandra <gepenkoalex@gmail.com>
2024-06-06 19:34:06 +0300

    added validation id

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -2,19 +2,22 @@ import { Router } from "express";
 // import { getAllContacts, getContactById } from "../services/contacts.js";
 import { createContactController, deleteContactByIdController, getContactByIdController, getContactsController, patchContactByIdController, putContactByIdController } from "../controllers/contacts.js";
 import { ctrlWrapper } from "../utils/ctrlWrapper.js";
+import { validateMongoId } from "../middlewares/validateMongoId.js";
 
 const contactsRouter = Router();
 
+// contactsRouter.use('/contacts/:contactId', validateMongoId('contactId'));
+
 contactsRouter.get('/contacts', ctrlWrapper(getContactsController));
 
-contactsRouter.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
+contactsRouter.get('/contacts/:contactId', validateMongoId('contactId'), ctrlWrapper(getContactByIdController));
 
 contactsRouter.post('/contacts', ctrlWrapper(createContactController));
 
-contactsRouter.patch('/contacts/:contactId', ctrlWrapper(patchContactByIdController));
+contactsRouter.patch('/contacts/:contactId', validateMongoId('contactId'), ctrlWrapper(patchContactByIdController));
 
-contactsRouter.put('/contacts/:contactId', ctrlWrapper(putContactByIdController));
+contactsRouter.put('/contacts/:contactId', validateMongoId('contactId'), ctrlWrapper(putContactByIdController));
 
-contactsRouter.delete('/contacts/:contactId', ctrlWrapper(deleteContactByIdController));
+contactsRouter.delete('/contacts/:contactId', validateMongoId('contactId'), ctrlWrapper(deleteContactByIdController));
 
 export default contactsRouter;
